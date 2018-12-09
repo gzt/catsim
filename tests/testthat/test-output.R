c21fd8e9035dba3d4aa91fbbf0396fc85ffe7197
gzt <gzthompson@gmail.com>
2018-12-09 00:03:26 -0600

    made tests a little faster

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -1,10 +1,10 @@
 context("test-output")
 
 
-x <- matrix(sample(1:4, 64^2, replace = TRUE), nrow = 64)
+x <- matrix(sample(1:4, 32^2, replace = TRUE), nrow = 32)
 y <- x
-for (i in 1:64) y[i, i] = 1
-for (i in 1:63) y[i, i + 1] = 1
+for (i in 1:32) y[i, i] = 1
+for (i in 1:31) y[i, i + 1] = 1
 
 test_that("Same input should have 1 as its result", {
   expect_equal(catmssim_2d(x,x), 1.0)
