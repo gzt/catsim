ea9e5a4dd4c7665b1826aaa44af70cd8dddb4d48
gzt <gzt@iastate.edu>
2019-07-10 16:40:59 -0500

    add 3d tests and test methods

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -7,8 +7,38 @@ for (i in 1:32) y[i, i] = 1
 for (i in 1:31) y[i, i + 1] = 1
 
 test_that("Same input should have 1 as its result", {
-  expect_equal(catmssim_2d(x,x, weights = 1), 1.0)
+    expect_equal(catmssim_2d(x,x, weights = 1), 1.0)
+    expect_equal(catmssim_2d(x,x, weights = 1, method = "Rand"), 1.0)
+    expect_equal(catmssim_2d(x,x, weights = 1, method = "Jaccard"), 1.0)
+    
 })
+
+test_that("Inputs are symmetric", {
+    expect_equal(catmssim_2d(x,y, weights = 1), catmssim_2d(y,x, weights = 1))
+      expect_equal(catmssim_2d(x,y, weights = c(.5,.5), method = "j"), catmssim_2d(y,x, weights = c(.5,.5), method = "j"))
+    expect_equal(catmssim_2d(x,y, weights = 1, method = "rand"), catmssim_2d(y,x, weights = 1, method = "rand"))
+
+
+    
+ set.seed(20181207)
+ dim = 16
+ x <- array(sample(1:4, dim^3, replace = TRUE), dim = c(dim,dim,dim))
+ y <- x
+ for (j in 1:dim){
+ for (i in 1:dim) y[i, i, j] = 1
+ for (i in 1:(dim-1)) y[i, i+1, j] = 1
+ }
+
+    expect_equal(catmssim_3d_slice(x,y, weights = c(.75,.25)), catmssim_3d_slice(y,x, weights = c(.75,.25)))
+    expect_equal(catmssim_3d_cube(x,y, weights = c(.75,.25), method = "j"), catmssim_3d_cube(y,x, weights = c(.75,.25), method = "j"))
+})
+
+
+set.seed(20181215)
+x <- matrix(sample(1:4, 32^2, replace = TRUE), nrow = 32)
+y <- x
+for (i in 1:32) y[i, i] = 1
+for (i in 1:31) y[i, i + 1] = 1
 
 test_that("Zero weights should have result of 1.0", {
   expect_equal(catmssim_2d(x,y, weights = c(0.0)), 1.0)
