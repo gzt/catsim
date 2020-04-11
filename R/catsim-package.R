5cb6fd0e20aa189f847ee1a067eac29a90e187df
gzt <gzthompson@gmail.com>
2020-04-10 23:50:33 -0500

    update description, add catsim-package docs

---
+++ R/catsim-package.R
@@ -0,0 +1,17 @@
+#' CatSIM: Binary and Categorical Image Structural Similarity Metric
+#'
+#' Computes a structural similarity metric (after the style of MS-SSIM for images)
+#' for binary and categorical 2D and 3D images. Can be based on accuracy (simple matching),
+#' Cohen's kappa, Rand index, adjusted Rand index, Jaccard index, Dice index, normalized
+#' mutual information, or adjusted mutual information. In addition, has fast computation
+#' of Cohen's kappa, the Rand indices, and the two mutual informations.
+#' @aliases catsim-package
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
