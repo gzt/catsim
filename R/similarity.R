3afd17c835b4ab2b3a0be549a144969b85b775b9
gzt <gzthompson@gmail.com>
2020-12-07 22:36:38 -0500

    fix URLS

--- R/similarity.R
+++ R/similarity.R
@@ -54,7 +54,7 @@
 #' Information Theoretic Measures for Clusterings Comparison:
 #' Variants, Properties, Normalization and Correction for Chance.
 #' J. Mach. Learn. Res. 11 (December 2010), 2837–2854.
-#' \url{http://www.jmlr.org/papers/v11/vinh10a}
+#' \url{https://www.jmlr.org/papers/v11/vinh10a}
 #' @export
 #'
 #' @examples
